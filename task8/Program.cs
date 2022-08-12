6b4ebf17c10ac47742142d7a63602d63aaaae9f5
Olga <shalamova_o@inbox.ru>
2022-08-12 10:48:26 +0500

    Add all folders

---
+++ task8/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая на вход принимает число N, 
+// а на выходе показывает все четные числа  от 1 до N
+// 5-> 2,4
+// 8> 2,4,6,8,
+
+Console.WriteLine("Введите целое число:");
+int number = Convert.ToInt32(Console.ReadLine());
+int counter = 1;
+
+while (counter <= number)
+    if (counter % 2 == 0)
+        Console.WriteLine(counter);
+    counter = counter+1;
+ // увы, так ничего и не заработало..
+
+  
